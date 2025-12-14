bc62bd8ecf190bfaafc2d68381399882c9fb962a
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-13 21:59:46 -0300

    feat: activating the validation on main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import * as admin from 'firebase-admin'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //activate the validation
+  app.useGlobalPipes(new ValidationPipe());
 
   admin.initializeApp({
     credential: admin.credential.cert('./firebase-config.json'),
