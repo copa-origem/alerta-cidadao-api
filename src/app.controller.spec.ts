a8d944d9cfb253727e119fda6012478bd2cd306a
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-25 20:21:20 -0300

    test: just ci/cd test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World test!"', () => {
+    it('should return "Hello World test! 2"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
