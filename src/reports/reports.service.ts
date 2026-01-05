47579e031eeb83010742bed1ef6abde7b65364fb
Rafael Rangel <rafael.profissional011@gmail.com>
2026-01-05 20:40:15 -0300

    feat: add the reports.service

---
+++ src/reports/reports.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class ReportsService {
+    constructor(
+        @Inject('rabbitmq') private readonly client: ClientProxy,
+    ) {}
+
+    async requestReport(userId: string, filters: any) {
+        const jobId = crypto.randomUUID();
+
+        const payload = {
+            jobId,
+            userId,
+            filters,
+            createdAt: new Date(),
+        };
+
+        this.client.emit('generate-report', payload);
+
+        return {
+            message: 'Solicitation recived. We gonna say when is ready',
+            jobId,
+            status: 'pending'
+        };
+    }
+}
