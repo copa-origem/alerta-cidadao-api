08c4c98a0a0a9829b054ba6ead4d669e8a1df738
Rafael Rangel <rafael.profissional011@gmail.com>
2026-01-07 20:17:30 -0300

    fix: fix the name on Inject

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,7 +4,7 @@ import { ClientProxy } from '@nestjs/microservices';
 @Injectable()
 export class ReportsService {
     constructor(
-        @Inject('rabbitmq') private readonly client: ClientProxy,
+        @Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy,
     ) {}
 
     async requestReport(userId: string, filters: any) {
