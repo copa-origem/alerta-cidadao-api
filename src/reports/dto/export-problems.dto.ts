0e5a3d0a9113a6cbbc428922a8730c301343ca55
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-19 20:54:58 -0300

    feat: the real verification on dto export problems

--- src/reports/dto/export-problems.dto.ts
+++ src/reports/dto/export-problems.dto.ts
@@ -1,14 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class ReportFiltersDto {
+    @ApiProperty({ example: 'OPEN', description: "Problem status to report"})
+    @IsString()
+    status: string;
+
+    @ApiProperty({ example: 'cc6b4a21-9a9f-43eb-aa93-c90d82e79677', description: "Category id"})
+    @IsString()
+    categoryId: string;
+
+    @ApiProperty({ example: '2025-12-01', description: "start date to report"})
+    @IsString()
+    startDate: string;
+
+    @ApiProperty({ example: '2026-01-01', description: "end date to report"})
+    @IsString()
+    endDate: string;
+}
 
 export class ExportReportDto {
-  @ApiProperty({
-    description: 'Filters apllied to generate the report',
-    example: {
-      status: 'OPEN',
-      categoryId: 'cc6b4a21-9a9f-43eb-aa93-c90d82e79677',
-      startDate: '2025-12-01',
-      endDate: '2026-01-01'
-    },
-  })
-  filters: any; 
+    @ApiProperty({ type: ReportFiltersDto })
+    @ValidateNested()
+    @Type(() => ReportFiltersDto)
+    filters: ReportFiltersDto;
 }
