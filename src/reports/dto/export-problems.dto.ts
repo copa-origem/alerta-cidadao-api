803e91a5372e8dd3dfa6b6cbb187b64c2b043027
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-19 20:31:55 -0300

    docs: adjust on property data on exports

--- src/reports/dto/export-problems.dto.ts
+++ src/reports/dto/export-problems.dto.ts
@@ -4,8 +4,8 @@ export class ExportReportDto {
   @ApiProperty({
     description: 'Filtros aplicados para a geração do relatório',
     example: {
-      status: 'PENDENTE',
-      category: 'Buracos',
+      status: 'OPEN',
+      categoryId: 'cc6b4a21-9a9f-43eb-aa93-c90d82e79677',
       startDate: '2025-12-01',
       endDate: '2026-01-01'
     },
