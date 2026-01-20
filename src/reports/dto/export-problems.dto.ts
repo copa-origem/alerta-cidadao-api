4256008b88a6064684f8ed9134cee5cb2603adec
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-19 21:03:23 -0300

    fix: fix the dto to export problems

--- src/reports/dto/export-problems.dto.ts
+++ src/reports/dto/export-problems.dto.ts
@@ -3,21 +3,25 @@ import { IsString, IsOptional, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 class ReportFiltersDto {
-    @ApiProperty({ example: 'OPEN', description: "Problem status to report"})
+    @ApiProperty({ example: 'OPEN', description: "Problem status to report", required: false})
+    @IsOptional()
     @IsString()
-    status: string;
+    status?: string;
 
-    @ApiProperty({ example: 'cc6b4a21-9a9f-43eb-aa93-c90d82e79677', description: "Category id"})
+    @ApiProperty({ example: 'cc6b4a21-9a9f-43eb-aa93-c90d82e79677', description: "Category id", required: false})
+    @IsOptional()
     @IsString()
-    categoryId: string;
+    categoryId?: string;
 
-    @ApiProperty({ example: '2025-12-01', description: "start date to report"})
+    @ApiProperty({ example: '2025-12-01', description: "start date to report", required: false})
+    @IsOptional()
     @IsString()
-    startDate: string;
+    startDate?: string;
 
-    @ApiProperty({ example: '2026-01-01', description: "end date to report"})
+    @ApiProperty({ example: '2026-01-01', description: "end date to report", required: false})
+    @IsOptional()
     @IsString()
-    endDate: string;
+    endDate?: string;
 }
 
 export class ExportReportDto {
